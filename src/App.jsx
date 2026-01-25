09eca8cbe341326125cf06ab8f41605655b04eb3
Darshik <darshikladhe182@gmail.com>
2026-01-25 18:26:00 +0530

    PROJECT COMPLETE

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,12 @@ import { VIEW } from "./constants";
 
 const App = () => {
   const [navTheme, setNavTheme] = useState("#000");
-  const [isOnGalleryNew, setIsOnGalleryNew] = useState(false);
-  const [isOnPokeView, setIsOnPokeView] = useState(false);
-
   const [view, setView] = useState(VIEW.HOME);
 
   return (
     <div>
-      <Nav
-        isOnPokeView={isOnPokeView}
-        setIsOnGallery={setIsOnGalleryNew}
-        view={view}
-        setView={setView}
-        bgColor={navTheme}
-        isOnGallery={isOnGalleryNew}
-      />
-      <FrontBanner
-        setIsOnPokeView={setIsOnPokeView}
-        isOnGallery={isOnGalleryNew}
-        setIsOnGallery={setIsOnGalleryNew}
-        updateNavTheme={setNavTheme}
-        view={view}
-        setView={setView}
-      />
+      <Nav view={view} setView={setView} bgColor={navTheme} />
+      <FrontBanner updateNavTheme={setNavTheme} view={view} setView={setView} />
     </div>
   );
 };
